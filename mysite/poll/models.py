f4ebd64a7fdc4b842434c4f270dad9803d39b5e8
vivekpradhanC <vivekpradhan.91@gmail.com>
2015-07-18 21:18:19 +0530

    first commit

---
+++ mysite/poll/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Question(models.Model):
+	def __str__(self):
+		return self.question_text
+	question_text=models.CharField(max_length=200)
+	pub_date=models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+	def __str__(self):
+		return self.choice_text
+	question=models.ForeignKey(Question)
+	choice_text=models.CharField(max_length=200)
+	votes=models.IntegerField(default=0)
